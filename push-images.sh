96a17c198b0914beda69eb38ae4cf467affc9e8d
Joxx0r <Joxxor@gmail.com>
2023-12-07 16:12:13 +0100

    implemented simpler pusher and build

---
+++ push-images.sh
@@ -0,0 +1,4 @@
+docker push europe-docker.pkg.dev/embark-shared/main/openmatch-synchronizer:1.8.0-joakim_test
+docker push europe-docker.pkg.dev/embark-shared/main/openmatch-query:1.8.0-joakim_test
+docker push europe-docker.pkg.dev/embark-shared/main/openmatch-frontend:1.8.0-joakim_test
+docker push europe-docker.pkg.dev/embark-shared/main/openmatch-backend:1.8.0-joakim_test

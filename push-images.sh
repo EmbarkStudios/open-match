c397af5d55d54f6bf455a19f5a0bffeb7c225c20
Joxx0r <Joxxor@gmail.com>
2023-12-07 16:17:20 +0100

    Improved push images path

--- push-images.sh
+++ push-images.sh
@@ -1,3 +1,5 @@
+export REGISTRY=europe-docker.pkg.dev/embark-shared/main
+make build-images
 docker push europe-docker.pkg.dev/embark-shared/main/openmatch-synchronizer:1.8.0-joakim_test
 docker push europe-docker.pkg.dev/embark-shared/main/openmatch-query:1.8.0-joakim_test
 docker push europe-docker.pkg.dev/embark-shared/main/openmatch-frontend:1.8.0-joakim_test
